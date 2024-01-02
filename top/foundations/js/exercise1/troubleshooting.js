cd1f327d88f6fb60d2b30adc468b323dae545d7d
Facu Garay <facus.garn@gmail.com>
2024-01-02 09:46:25 -0300

    feat: Add js foundations exerecises

---
+++ top/foundations/js/exercise1/troubleshooting.js
@@ -0,0 +1,16 @@
+/**
+ * ===== Troubleshooting =====
+ * The function below should log the number 2, however it does not, 
+ * see if you can fix it!
+ * Be sure to fix it in the spirit of the code, do not hard code the result.
+ */
+/**
+ * return the sum of the two numbers 
+ * @param {*} number1 
+ * @param {*} number2 
+ * @returns 
+ */
+const add = (number1, number2) => number1 + number2;
+
+// export the add function
+module.exports = add;
